95377dacd8aff940e2ac5f1eba9f10621fcd536f
Will <jiepeng.huwu@sjsu.edu>
2017-01-25 15:35:06 -0800

    added association plan/user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125231926) do
+ActiveRecord::Schema.define(version: 20170125233129) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "first_name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170125231926) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "plan_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
