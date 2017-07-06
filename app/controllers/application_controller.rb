37eb6a82a1d14bfec35a77201e1b59bc805969f7
Will <jiepeng.huwu@sjsu.edu>
2017-07-05 20:06:40 -0700

    Fixed errors in app_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_filter :configure_permitted_parameters, if: devise_controller?
-  
-  protected
-  	  # white-listing parameters. Securely allow people to submit things thru the web
-	  def configure_permitted_parameters
-	  	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :stripe_card_token, :email, :password, :password_confirmation)}
-	  end
+	before_filter :configure_permitted_parameters, if: :devise_controller?
+
+	protected 
+		def configure_permitted_parameters
+			devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :stripe_card_token, :email, :password, :password_confirmation])
+		end
 end
+	
